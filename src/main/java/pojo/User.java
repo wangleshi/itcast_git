af26dcbb40088afb6d98817880cef979d7106dae
lisi666 <1150100934@qq.com>
2020-09-01 17:50:15 +0800

    firstcommit

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,35 @@
+package pojo;
+
+/**
+ * @description:
+ * @author:关崇波
+ * @date:2020/9/117:39
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
