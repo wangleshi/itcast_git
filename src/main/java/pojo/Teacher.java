1f6b8c2986611259c388f86f242e3af15c9b01e4
lisi666 <1150100934@qq.com>
2020-09-14 20:29:52 +0800

    添加Teacher

---
+++ src/main/java/pojo/Teacher.java
@@ -0,0 +1,13 @@
+package pojo;
+
+import javax.naming.Name;
+
+/**
+ * @description:
+ * @author:关崇波
+ * @date:2020/9/1420:28
+ */
+public class Teacher {
+    private String name;
+    private int age;
+}
